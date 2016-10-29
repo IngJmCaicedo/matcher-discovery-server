1d311ce915084d4c9548fe03b7a72ae34711fc10
Juan Manuel Caicedo <jmcaicedo1@gmail.com>
2016-10-28 20:48:23 -0500

    Docker configuration

---
+++ src/test/java/ingjmcaicedo/matcher/discovery/test/DicoveryServerConfigTest.java
@@ -0,0 +1,19 @@
+package ingjmcaicedo.matcher.discovery.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import ingjmcaicedo.matcher.discovery.DiscoveryServerConfig;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes=DiscoveryServerConfig.class)
+public class DicoveryServerConfigTest {
+
+	@Test
+	public void start_context(){
+		
+	}
+	
+}
